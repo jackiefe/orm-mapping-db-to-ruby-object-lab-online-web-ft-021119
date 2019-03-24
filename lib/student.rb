f51a2b20980a120e4aa6ffead285277541eb6feb
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-24 21:56:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -112,10 +112,10 @@ class Student
     end.first 
   end 
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(X) 
     sql =<<-SQL
     SELECT *
-    FROM students WHERE grade = X 
+    FROM students WHERE grade = ?  
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
