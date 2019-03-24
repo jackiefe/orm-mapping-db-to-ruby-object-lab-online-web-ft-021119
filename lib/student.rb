41ec50a8fb411e9ccc82976991de1eebd9273783
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-24 21:49:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,4 +100,15 @@ class Student
       self.new_from_db(row)
     end 
   end 
+  
+  def self.first_student_in_grade_10 
+    sql =<<-SQL 
+    SELECT * 
+    FROM students WHERE grade = 10
+    ORDER BY (name)ASC LIMIT 1 
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end 
 end
