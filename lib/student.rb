8775930a0b28a4055a78b65d5844fe9425b49ed5
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-24 21:43:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,7 +94,7 @@ class Student
     SELECT *
     FROM students
     WHERE grade = 10
-    LIMIT 1
+    ORDER BY (name)ASC LIMIT 1
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
