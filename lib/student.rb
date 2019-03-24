950ef7ce9f2c33d93bf6ee4690f8a90792d1a99b
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-24 21:25:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,7 @@ class Student
   def self.students_below_12th_grade
     sql =<<-SQL
     SELECT * FROM students 
-    WHERE grade < 12   
+    WHERE grade < 12  name.first  
     SQL
     DB[:conn].execute(sql)
   end 
