798da3587408268cd83ec392c3d3179085cbeeb8
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-24 22:00:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -117,8 +117,7 @@ class Student
     SELECT *
     FROM students WHERE grade = ?
     SQL
-    DB[:conn].execute(sql,x).map do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql,x)
     end.first 
   end 
 end
